d077212b339c35db756bb16511c85b0b9487e0a3
xiaozisong <xiaozis@gantsoftware.com>
2023-08-03 23:50:16 +0800

    feat: 实现context的数据结构

--- packages/shared/ReactSymbols.ts
+++ packages/shared/ReactSymbols.ts
@@ -4,6 +4,13 @@ export const REACT_ELEMENT_TYPE = supportSymbol
 	? Symbol.for('react.element')
 	: 0xeac7;
 
+export const REACT_CONTEXT_TYPE = supportSymbol
+	? Symbol.for('react.context')
+	: 0xeacc;
 export const REACT_FRAGMENT_TYPE = supportSymbol
 	? Symbol.for('react.fragment')
-	: 0xeacb;
+	: 0xeac2;
+
+export const REACT_PROVIDER_TYPE = supportSymbol
+	? Symbol.for('react.provider')
+	: 0xeac2;
